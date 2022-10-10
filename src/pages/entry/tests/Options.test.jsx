6d1c4d52d864d87d0a2eb6fd65028cd4ab0f4cf8
Mark Wlodawski <expresso@expresso.local>
2022-10-09 20:42:49 -0400

    Topping option quiz

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Options from "../Options";
+import ToppingOption from "../ToppingOption";
 
 test("displays image for each scoop option from server", async () => {
   render(<Options optionType="scoops" />);
@@ -11,4 +12,18 @@ test("displays image for each scoop option from server", async () => {
   // confirm alt text of images
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+test("displays image for each topping option from server", async () => {
+  render(<Options optionType="toppings" />);
+
+  // find images
+  const toppingImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  // confirm alt text of images
+  const imageTitles = toppingImages.map((img) => img.alt);
+  expect(imageTitles).toEqual(["Cherries", "M&Ms", "Hot fudge"]);
 });
