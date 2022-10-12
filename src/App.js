33e1837c5641911d5d219e389f26ab6bacffc2e8
Mark Wlodawski <expresso@expresso.local>
2022-10-12 19:12:36 -0400

    Add code for sundae app components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import "./App.css";
+import { Container } from "react-bootstrap";
+import OrderEntry from "../src/pages/entry/OrderEntry";
+import { OrderDetailsProvider } from "./contexts/OrderDetails";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Container>
+      <OrderDetailsProvider>
+        {/* Summary page and entry page need provider */}
+        <OrderEntry />
+      </OrderDetailsProvider>
+      {/* Confirmation page does not need provider */}
+    </Container>
+  );
 }
 
 export default App;
